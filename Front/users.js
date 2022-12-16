37cf0045291c5b4dbaa04adc573f349e7bf53bbe
TheuTheuutra <105327246+TheuTheuutra@users.noreply.github.com>
2022-12-15 22:36:56 -0300

    Quarto Commit

--- Front/users.js
+++ Front/users.js
@@ -2,19 +2,18 @@ const url = 'http://localhost:8080/users';
 let userslist;
 
 function userscreate(){
-    const nickname = $("nickname-c").val();
-    const email = $("email-c").val();
-    const password = $("password-c").val();
-    const body = `{"nickName": "${nickname}"}`;
+    const nickname = $("#nickname-c").val();
+    const email = $("#email-c").val();
+    const password = $("#password-c").val();
+    const body = `{"nickName": "${nickname}", "email": "${email}", "password": "${password}"}`;
     
+    console.log(body)
     $.ajax({
         type:"POST",
         url: url,
         data: body,
-        sucess: (res) => {
+        success: (res) => {
             console.log('post done.')
-
-
         },
         contentType: "application/json",
         dataType: "json"
